8f63d46ffbd88344986e216d73dc404dda43054a
«Kristina» <kristinabol1996@gmail.com>
2022-08-18 02:43:22 +0800

    1 задача на сумму чисел от 1 до А

---
+++ Task001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+int Sum(int a)
+{
+    int result = 0;
+    for (int number = 0; number <= a; number++)
+    {
+        result += number;
+    }
+    return result;
+}
+
+Console.WriteLine(Sum(5));
+Console.WriteLine(Sum(1));
+
+void PrintSum(int a)
+{
+    Console.WriteLine($"Сумма чисел от 1 до {a} равна {Sum(a)}");
+}
+
+PrintSum(5);
+PrintSum(1);

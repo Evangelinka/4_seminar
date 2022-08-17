dd34dafb9df7dbcd474113aecc989690538a1e88
«Kristina» <kristinabol1996@gmail.com>
2022-08-18 02:44:39 +0800

    2 задача, количество цифр в числе

---
+++ Task002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая принимает на вход число, и выдает количество цифр в числе
+
+int NumberDigits(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count += 1;
+    }
+    return count;
+}
+
+void printNumberDigits(int number)
+{
+    Console.WriteLine($"Количество цифр в числе {number} равна {NumberDigits(number)}");
+}
+
+printNumberDigits(1234);
+printNumberDigits(12345678);
+printNumberDigits(1);
+

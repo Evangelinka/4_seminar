7a27654b892226b24ed415b9c26baa7ad2e38897
«Kristina» <kristinabol1996@gmail.com>
2022-08-18 02:46:42 +0800

    4 задача, создание массива из 0 и 1

---
+++ Task004/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая выводит массив из 8 элементов, заполненных нулями 
+// и единицами в случайном порядке. [1, 0, 1, 1, 0, 1, 0, 0].
+
+int[] ArrayZerosOnes(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+    return array;
+}
+void PrintArrayZerosOnes(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArrayZerosOnes(ArrayZerosOnes(8));

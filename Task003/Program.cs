1f3a877f199cbb0920aab0845fbb2b1c1078e110
«Kristina» <kristinabol1996@gmail.com>
2022-08-18 02:45:40 +0800

    3 задача, факториал

---
+++ Task003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+
+int ProductNumbers(int N)
+{
+    int result = 1;
+    for (int number = 1; number <= N; number++)
+    {
+        result *= number;
+    }
+    return result;
+}
+
+void PrintProductNumbers(int N)
+{
+    Console.WriteLine($"Произведение чисел от 1 до {N} равно {ProductNumbers(N)}");
+}
+
+PrintProductNumbers(4);
+PrintProductNumbers(5);
